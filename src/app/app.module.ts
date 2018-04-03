4378fb0d36fc4acc62035088cfd86ca6093fb884
Aderbal Farias <aderbalfarias@hotmail.com>
2018-04-03 20:09:50 -0300

    Including new component with add and list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
 import { ExampleComponent } from './components/example/example.component';
 import { CliexamplesComponent } from './components/cliexamples/cliexamples.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserComponent,
-    ExampleComponent,
-    CliexamplesComponent,
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        UserComponent,
+        UsersComponent,
+        ExampleComponent,
+        CliexamplesComponent,
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../models/User';
+
+@Component({
+    selector: 'app-users',
+    templateUrl: './users.component.html',
+    styleUrls: ['./users.component.css']
+})
+
+export class UsersComponent implements OnInit {
+    users: User[];
+    showExtended: boolean = true;
+    loaded: boolean = false;
+
+    constructor() { }
+
+    ngOnInit() {
+        this.users = [
+            {
+                firstName: 'John',
+                lastName: 'Doe',
+                age: 30,
+                address: {
+                    street: '50 Main st',
+                    city: 'Boston',
+                    state: 'MA'
+                }
+            },
+            {
+                firstName: 'Kevin',
+                lastName: 'Johnson',
+                age: 34,
+                address: {
+                    street: '20 School st',
+                    city: 'Lynn',
+                    state: 'MA'
+                }
+            },
+            {
+                firstName: 'Karen',
+                lastName: 'Williams',
+                age: 26,
+                address: {
+                    street: '55 Mill st',
+                    city: 'Miami',
+                    state: 'FL'
+                }
+            }
+        ];
+
+        this.loaded = true;
+
+        this.addUser({
+            firstName: 'David',
+            lastName: 'Jackson'
+        });
+    }
+
+    addUser(user: User) {
+        this.users.push(user);
+    }
+}
