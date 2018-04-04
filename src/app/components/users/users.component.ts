b875b6be049d7dd7c268d1050f41b66ee8070a16
Aderbal Farias <aderbalfarias@hotmail.com>
2018-04-04 01:22:57 -0300

    using resources of the angular

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -12,6 +12,9 @@ export class UsersComponent implements OnInit {
     users: User[];
     showExtended: boolean = true;
     loaded: boolean = false;
+    enableAdd: boolean = true;
+    currentClasses = {};
+    currentStyles = {};
 
     constructor() { }
 
@@ -20,12 +23,14 @@ export class UsersComponent implements OnInit {
             {
                 firstName: 'John',
                 lastName: 'Doe',
-                age: 30,
+                age: 70,
                 address: {
                     street: '50 Main st',
                     city: 'Boston',
                     state: 'MA'
-                }
+                },
+                image: '../../assets/images/img1.jpg',
+                isActive: true
             },
             {
                 firstName: 'Kevin',
@@ -35,7 +40,9 @@ export class UsersComponent implements OnInit {
                     street: '20 School st',
                     city: 'Lynn',
                     state: 'MA'
-                }
+                },
+                image: '../../assets/images/img2.jpg',
+                isActive: false
             },
             {
                 firstName: 'Karen',
@@ -45,19 +52,38 @@ export class UsersComponent implements OnInit {
                     street: '55 Mill st',
                     city: 'Miami',
                     state: 'FL'
-                }
+                },
+                image: '../../assets/images/img3.jpg',
+                isActive: true
             }
         ];
 
         this.loaded = true;
 
-        this.addUser({
-            firstName: 'David',
-            lastName: 'Jackson'
-        });
+        // this.addUser({
+        //   firstName: 'David',
+        //   lastName: 'Jackson'
+        // });
+
+        this.setCurrentClasses();
+        this.setCurrentStyles();
     }
 
     addUser(user: User) {
         this.users.push(user);
+    }
+
+    setCurrentClasses() {
+        this.currentClasses = {
+            'btn-success': this.enableAdd,
+            'big-text': this.showExtended
+        }
+    }
+
+    setCurrentStyles() {
+        this.currentStyles = {
+            'padding-top': this.showExtended ? '0' : '40px',
+            'font-size': this.showExtended ? '' : '40px'
+        }
     }
 }
