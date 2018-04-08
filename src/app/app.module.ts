8ff6bee045219e1b8f908c91b225dd51b70d2b91
Aderbal Farias <aderbalfarias@hotmail.com>
2018-04-08 15:03:48 -0300

    Including routes and new structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import { UserService } from './services/user.service';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostService } from './services/post.service';
 import { PostFormComponent } from './components/post-form/post-form.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { PostComponent } from './components/post/post.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
     declarations: [
@@ -25,12 +29,16 @@ import { PostFormComponent } from './components/post-form/post-form.component';
         CliexamplesComponent,
         NavbarComponent,
         PostsComponent,
-        PostFormComponent
+        PostFormComponent,
+        HomeComponent,
+        PostComponent,
+        NotFoundComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        HttpClientModule
+        HttpClientModule,
+        AppRoutingModule
     ],
     providers: [UserService, PostService],
     bootstrap: [AppComponent]
